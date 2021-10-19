71e42cd2f46771afe909324a56551856d9219f55
Farid Mitri <faridmitri@gmail.com>
2021-10-19 09:32:36 +0300

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,9 +1,7 @@
-"""
-module
-"""
-import os
+import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,16 +17,20 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-def english_to_french(english_text):
+def EnglishToFrench(english_text):
+    if english_text== '':
+        return ''
     translation = language_translator.translate(
-    text=english_text,
+    english_text,
     model_id='en-fr').get_result()
-    french_text = translation.get("translations")[0]["translation"]
+    french_text=translation['translations'][0]['translation']
     return french_text
 
-def french_to_english(french_text):
+def FrenchToEnglish(french_text):
+    if french_text== '':
+        return ''
     translation = language_translator.translate(
-    text=french_text,
+    french_text,
     model_id='fr-en').get_result()
-    english_text = translation.get("translations")[0]["translation"]
+    english_text=translation['translations'][0]['translation']
     return english_text
