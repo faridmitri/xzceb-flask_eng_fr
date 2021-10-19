0e25159bc6b6e223c52c32ae0fe86b0a760a297e
Farid Mitri <faridmitri@gmail.com>
2021-10-19 11:42:41 +0300

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -5,31 +5,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+APIKEY = os.environ['apikey']
+URL = os.environ['url']
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator(APIKEY)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url(url)
+language_translator.set_service_url(URL)
 
-def EnglishToFrench(english_text):
-    if english_text== '':
+def EnglishToFrench(englishText):
+    if englishText == '':
         return ''
-    translation = language_translator.translate(
-    english_text,
-    model_id='en-fr').get_result()
-    french_text=translation['translations'][0]['translation']
-    return french_text
-
-def FrenchToEnglish(french_text):
-    if french_text== '':
+    translation = language_translator.translate(englishText, model_id='en-fr').get_result()
+    frenchText=translation['translations'][0]['translation']
+    return frenchText
+    
+def FrenchToEnglish(frenchText):
+    if frenchText == '':
         return ''
-    translation = language_translator.translate(
-    french_text,
-    model_id='fr-en').get_result()
-    english_text=translation['translations'][0]['translation']
-    return english_text
+    translation = language_translator.translate(frenchText, model_id='fr-en').get_result()
+    englishText=translation['translations'][0]['translation']
+    return englishText
